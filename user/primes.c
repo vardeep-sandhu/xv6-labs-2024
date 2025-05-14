6e292d9efa2f9e3761e73a9f8fe62815dea13517
vardeep-sandhu <vardeep.singh.sandhu@gmail.com>
2025-05-14 13:40:12 +0200

    added woriking solution of primes[with bug that the program terminates after a few calls]

--- user/primes.c
+++ user/primes.c
@@ -2,57 +2,63 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int primes(int* list_nums, int counter){
+void primes(int read_fd){
     // stopping condition
-    if (counter == 0){
-        return 0;
+    int prime_num = 0;
+    if (read(read_fd, &prime_num, 4) != 4){
+        close(read_fd);
+        return;
     }
-    int prime_num = list_nums[0];
-    // prime found
-    printf("prime: %d\n", prime_num);
+    printf("prime %d\n", prime_num);
 
-    // filtering of array
+    int p[2];
+    pipe(p);
+    int pid = fork();
+    if (pid == 0){
+        // reading side which recurces
+        close(p[1]); 
+        primes(p[0]);
+        exit(0);
+    }
+    else{
+        // writing side which filters
+
+        // parent
+        close(p[0]); // close reading end
+        
+        int num = 0;
+        while(read(read_fd, &num, 4) == 4){
+            if (num % prime_num != 0){
+                write(p[1], &num, 4);
+            }
+        }
+        close(p[1]);
+        close(read_fd);
+        wait(0);
+    }
+    return ;
+}
+
+int main(){
     int p[2];
     pipe(p);
     int pid = fork();
     
     if (pid == 0){
-        close(p[1]); //close write
-        int num = 0;
-        counter = 0;
-        memset(list_nums, 0, sizeof(*list_nums));
-        
-        while(read(p[0], &num, 4) == 4){
-            if (num % prime_num != 0){
-                counter ++;
-                list_nums[counter] = num;
-            }
-        }
-        printf("%d\n", counter);
+        close(p[1]);
+        primes(p[0]);
         close(p[0]);
-        primes(list_nums, counter);
         exit(0);
     }
     else{
+    // parent writes
+    close(p[0]); // close read part
 
-        // only writes
-        close(p[0]); //close read
-        for (int i = 0; i < counter; i++){
-            write(p[1], &list_nums[i], 4);
-        }
-        close(p[1]);
-        wait(0);
+    for(int i = 2; i <= 280; i++){
+            write(p[1], &i, 4);
     }
-return 0;
-}
-
-int main(){
-    int test_list[280];
-    int counter = 0;
-    for(int i = 0; i <= 278; i++){
-        test_list[i] = i + 2;
-        counter ++;
+    close(p[1]);
+    wait(0);
     }
-    // create a pipe
-    primes(test_list, counter);
+exit(0);
 }
